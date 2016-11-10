2b1019875c523e3f6551fa702173fc52fc351b43
Marlena Bowen <marlena@joinroot.com>
2016-11-10 14:42:17 -0500

    Adds tests and method

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -1,4 +1,12 @@
 class StringCalculator 
+  def self.add(string)
+    return 0 if string == “”
+    check_for_negative_numbers(string)
+  end 
+  self.check_for_negative_numbers(string)
+    if string.include?(“-”)
+        raise “negatives not allowed”
+    end 
+  end
 end
-
 
--- string_calculator/spec/lib/string_calculator_spec.rb
+++ string_calculator/spec/lib/string_calculator_spec.rb
@@ -3,5 +3,11 @@ require "string_calculator.rb"
 
 describe StringCalculator do
   describe ".add" do
+    it “returns 0 when string is empty” do 
+      expect(described_class.add(“”).to eq(0)
+    end 
+    it “raises an error if the string contains negative numbers” do 
+      expect {described_class.add(“-1”).to raise_error(“negatives not allowed”)
+    end
   end
 end
