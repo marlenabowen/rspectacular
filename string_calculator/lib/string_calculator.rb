316f2c8aaa43337c010016e533f5193e76e97072
Bob Carson <bob@carvinesoftware.com>
2016-11-13 16:14:27 -0500

    Replaces unicode quotes with regular quotes.

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -1,12 +1,12 @@
-class StringCalculator 
+class StringCalculator
   def self.add(string)
-    return 0 if string == “”
+    return 0 if string == ""
     check_for_negative_numbers(string)
-  end 
+  end
   self.check_for_negative_numbers(string)
-    if string.include?(“-”)
-        raise “negatives not allowed”
-    end 
+    if string.include?("-")
+        raise "negatives not allowed"
+    end
   end
 end
 
